443c48022dc24d402340cf1d8513d79fff3a3889
Francisco <franciscogm31@gmail.com>
2021-08-26 01:53:39 -0400

    deployment 2

--- database/database.js
+++ database/database.js
@@ -1,15 +1,22 @@
 const Sequalize = require("sequelize");
 
+const uri = process.env.DATABASE_URI;
 const host = process.env.DATABASE_HOST || "localhost";
 const database = process.env.DATABASE_NAME || "skatepark";
 const puerto = process.env.DATABASE_PUERTO || 5432;
 const usuario = process.env.DATABASE_USUARIO || "postgres";
 const contraseña = process.env.DATABASE_CONTRASENA || "postgres";
 
-module.exports = new Sequalize(database, usuario, contraseña, {
+module.exports = new Sequalize(uri, {
   host: host,
   dialect: "postgres",
   port: puerto,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   pool: {
     max: 5,
     min: 0,
